033025c3459a7c34750c7b3cae056ecda759e415
Barbara Dalena <bdalena@dacmpch147.extra.cea.fr>
2022-08-25 11:26:11 +0200

    Bash script to run seeds in sequence

---
+++ run_orbit_cpymad_seed.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for de in `seq 1 2`; do
+    echo "$de" 
+    sed -e "s/eseed=[0-9][0-9]/eseed=$de/g" test_cpymadx_tds.py > tmp.py
+    python3 tmp.py > seed-"$de".out
+    mv *.tab test_seed"$de"/
+    #rm tmp*.py
+done
