4bb12eb2375abac5c61df4e17275ff1df170f49e
Barbara Dalena <bdalena@dacmpch147.extra.cea.fr>
2022-08-29 10:13:26 +0200

    fix bug for running for different seeds*

--- run_orbit_cpymad_seed.sh
+++ run_orbit_cpymad_seed.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-for de in `seq 1 10`; do
+for de in `seq 1 100`; do
     echo "$de" 
     sed -e "s/eseed=[0-9][0-9]/eseed=$de/g" test_cpymadx_tds.py > tmp.py
+    sed -e "s/seed=[0-9][0-9];/seed=$de;/g" select_errors_tmp.madx > select_errors.madx
     python3 tmp.py > seed-"$de".out
     mv *.tab test_seed"$de"/
-    rm tmp*.py
+    rm tmp*.py select_errors.madx
 done
