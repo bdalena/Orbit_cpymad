61b10d14d7581c1b63bb8d808ac86d0cbc69f4aa
Tamatoumi <tatiana.dasilva@icloud.com>
2022-09-13 13:56:51 +0200

    tune match added

--- test_cpymadx_tds.py
+++ test_cpymadx_tds.py
@@ -2,12 +2,11 @@
 # Tatiana Da Silva - CEA
 #-----------------------------------------------------------------------------------------
 
+
+
 from cpymad.madx import Madx
 import matplotlib.pyplot as plt
 from numpy.fft import fft, ifft
-from scipy.stats import norm
-from scipy.fftpack import fft
-from scipy.fftpack import fftfreq
 import def_functions2 as df
 import numpy as np
 import pandas as pd
@@ -90,7 +89,7 @@ dqx_tar=0
 dqy_tar=0
           
 #----- ERRORS -----
-madx.call(file='select_errors.madx')
+madx.call(file='select_errors.madx') #!!!!!!!!!!!!
 
 #----- SAVE ERRORS ----
 madx.option(echo=True)
@@ -102,32 +101,10 @@ madx.twiss(sequence='fcc_heb', BETA0='BETAIP', file='test_seed{0}/FCCee_heb_mode
 
 #----- ANALYTICAL CORRECTORS CALCULATION -----
 '''
-file1="FCCee_heb_modett.tfs"
-file2="FCCee_heb_errors_corr.out"
+file1='test_seed{0}/FCCee_heb_modett_seed{0}.tfs'.format(eseed)
+file2='test_seed{0}/FCCee_heb_errors_corr_seed{0}.out'.format(eseed)
 
-df.anal_corr_calc(madx,file1,file2)
-
-#----- CORRECT TUNES & CHROMA -----
-#twiss_err=madx.twiss(sequence='fcc_heb', file='FCCee_heb_modett_err.tfs')
-#T_err=df.save_twiss(twiss_err)
-
-print('GLOBAL')
-print(madx.globals['k.mcbh.a1.001'])
-print(madx.globals['k.mcbh.a1.002'])
-print(madx.globals['k.mcbv.a1.001'])
-print(madx.globals['k.mcbv.a1.002'])
-
-madx.twiss(sequence='fcc_heb',BETA0='BETAIP1',file='FCCee_heb_modett_cor.tfs') #line
-#madx.twiss(sequence='fcc_heb',file='FCCee_heb_modett_cor.tfs') #ring
-'''
-
-#----- MATCHING -----
-'''
-#tune match
-df.tune_match('fcc_heb',1.0E-7)
-
-#chroma match
-df.chroma_match('fcc_heb',1.0E-7)
+df.anal_corr_calc(file1,file2)
 '''
 
 #----- CORRECTION -----
@@ -161,6 +138,34 @@ madx.twiss(BETA0='BETAIP', sequence='fcc_heb', file='test_seed{0}/FCCee_heb_mode
 df.svd_ring(madx,'fcc_heb',0,tol_err)
 madx.twiss(BETA0='BETAIP', sequence='fcc_heb', file='test_seed{0}/FCCee_heb_modett_orbcor_all_sextuon_it2_seed{0}.tfs'.format(eseed))
 
+os.system('mv cx_fccee_heb_mic_all.tab cx_fccee_heb_mic_all_line_it2.tab')
+os.system('mv cy_fccee_heb_mic_all.tab cy_fccee_heb_mic_all_line_it2.tab')
+
+#----- MATCHING -----
+
+df.match_quad(madx,'FCCee_heb-quads.str')
+
+madx.call(file='FCCee_heb-quads.str')
+
+#tune match
+df.tune_match('fcc_heb',1.0E-7,tol_tar)
+
+madx.twiss(sequence='fcc_heb', file='test_seed{0}/FCCee_heb_modett_tune_match_seed{0}.tfs'.format(eseed))
+
+#iteration n°1
+df.svd_ring(madx,'fcc_heb',0,tol_err)
+madx.twiss(sequence='fcc_heb', file='test_seed{0}/FCCee_heb_modett_orbcor_all_sextuon_tune_match_it1_seed{0}.tfs'.format(eseed))
+
+#iteration n°2
+df.svd_ring(madx,'fcc_heb',0,tol_err)
+madx.twiss(sequence='fcc_heb', file='test_seed{0}/FCCee_heb_modett_orbcor_all_sextuon_tune_match_it2_seed{0}.tfs'.format(eseed))
+
+#iteration n°3
+df.svd_ring(madx,'fcc_heb',0,tol_err)
+madx.twiss(sequence='fcc_heb', file='test_seed{0}/FCCee_heb_modett_orbcor_all_sextuon_tune_match_it3_seed{0}.tfs'.format(eseed))
+
+os.system('mv cx_fccee_heb_mic_all.tab cx_fccee_heb_mic_all_ring_it3.tab')
+os.system('mv cy_fccee_heb_mic_all.tab cy_fccee_heb_mic_all_ring_it3.tab')
 
 
 
